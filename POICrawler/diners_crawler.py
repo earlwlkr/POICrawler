4151bc60502ea404e0e87bd5063b4fb2c40329d6
earlwlkr <mike358015@gmail.com>
2015-05-01 10:05:47 +0700

    Add foody.vn crawler, disable DDAOCrawler

--- POICrawler/diners_crawler.py
+++ POICrawler/diners_crawler.py
@@ -1,7 +1,7 @@
 import os
 import re
-import traceback
-from bs4 import BeautifulSoup   # external lib
+import json
+from bs4 import BeautifulSoup
 
 from POICrawler.diner import Diner
 from POICrawler.requester import Requester
@@ -18,83 +18,136 @@ class DinerCrawler(object):
         raise NotImplementedError
 
 
-class DDAOCrawler(DinerCrawler):
+class FoodyVNCrawler(DinerCrawler):
 
     def __init__(self):
-        super(DDAOCrawler, self).__init__()
+        super(FoodyVNCrawler, self).__init__()
 
-    # Returns all Diners from URL response.
-    # It gets link from the 'desc' div,
-    # goes to link and get diner's details.
-    def extract_page_data(self, response):
-        main_soup = BeautifulSoup(response.text)
-        diners = []
-
-        for item in main_soup.find_all('div', class_='desc'):
-            if item.h2 is None:
-                continue
-
-            anchor = item.h2.a
-            link = anchor['href']
-
-            details_response = self.session.get(link)
-            yield self.get_diner_details(details_response)
-
-    # Returns a Diner from the details page.
-    def get_diner_details(self, details_response):
-        soup = BeautifulSoup(details_response.text)
-
-        name = soup.find('h1', class_='place-detail-title').string
-
-        description = soup.find('div', class_='desc')
-
-        anchor = description.find('a', class_='place-category')
-        category = anchor.string
-
-        # Has to call next_sibling 2 times because:
-        # http://www.crummy.com/software/BeautifulSoup/bs4/doc/#next-sibling-and-previous-sibling
-        anchor = anchor.next_sibling.next_sibling
-        address = anchor.span.next.string.strip()
-
-        anchor = anchor.next_sibling.next_sibling
-        phone = anchor.span.next.string.strip()
-
-        anchor = description.find('div', class_='block')
-        open_time = anchor.find('ul', class_='bullet').li.string
-
-        # Skip phần comment (<!-- /.block -->)
-        anchor = anchor.next_sibling.next_sibling.next_sibling
-        price_range = anchor.find('strong').string
-
-        return Diner(name, address, phone,
-                     category, open_time, price_range)
+    def get_sub_category(self, id):
+        if id == 1:
+            return 'Nhà hàng'
+        if id == 2:
+            return 'Cà phê/Kem'
+        if id == 3:
+            return 'Quán ăn'
+        return 'Không tìm thấy ' + str(id)
 
     def crawl(self):
-        data = {
-            'ajax': 1,
-            'offset': 0,
-            'areaid': 3,
-            'areaseo': 'tp-ho-chi-minh'
-        }
 
-        offset = 0
-        count = 0
+        data = {
+            'page': 1
+        }
+        headers = {
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+        url = 'http://www.foody.vn/ho-chi-minh/dia-diem'
 
         while True:
-            response = self.session.post(
-                'http://diadiemanuong.com/location/ajaxLoadMore/', data=data)
-
+            response = self.session.get(url, headers=headers, params=data)
             if response.status_code != 200:
-                break
+                return
 
-            for diner in self.extract_page_data(response):
-                count += 1
-                yield diner
+            restaurants = json.loads(response.text)['restaurants']
 
-                if count % 10 == 0:
-                    print('{} diners collected'.format(count))
-                if count >= 30:
-                    return
+            for restaurant in restaurants:
+                name = restaurant['Name']
+                address = restaurant['Address'] + ' ' +\
+                    restaurant['District'] + ' ' + restaurant['City']
+                phone = restaurant['Phone']
+                category = self.get_sub_category(restaurant['MainCategoryId'])
 
-            offset += 10
-            data['offset'] = offset
+                link = 'http://www.foody.vn' + restaurant['DetailUrl']
+                print('Requesting page {}'.format(link))
+                response = self.session.get(link)
+                soup = BeautifulSoup(response.text)
+                try:
+                    open_time = soup.find('span', attrs={'itemprop': 'opens'}).text + \
+                        ' - ' + \
+                        soup.find('span', attrs={'itemprop': 'closes'}).text
+
+                    price_range = soup.find(
+                        'span', attrs={'itemprop': 'priceRange'}).find('span').text
+
+                except AttributeError:
+                    print('Error with ' + link)
+                    continue
+
+                yield Diner(name, address, phone,
+                            category, open_time, price_range)
+
+            data['page'] += 1
+
+
+# class DDAOCrawler(DinerCrawler):
+
+#     def __init__(self):
+#         super(DDAOCrawler, self).__init__()
+
+#     # Returns all Diners from URL response.
+#     # It gets link from the 'desc' div,
+#     # goes to link and get diner's details.
+#     def extract_page_data(self, response):
+#         main_soup = BeautifulSoup(response.text)
+#         diners = []
+
+#         for item in main_soup.find_all('div', class_='desc'):
+#             if item.h2 is None:
+#                 continue
+
+#             anchor = item.h2.a
+#             link = anchor['href']
+
+#             details_response = self.session.get(link)
+#             yield self.get_diner_details(details_response)
+
+#     # Returns a Diner from the details page.
+#     def get_diner_details(self, details_response):
+#         soup = BeautifulSoup(details_response.text)
+
+#         name = soup.find('h1', class_='place-detail-title').string
+
+#         description = soup.find('div', class_='desc')
+
+#         anchor = description.find('a', class_='place-category')
+#         category = anchor.string
+
+#         # Has to call next_sibling 2 times because:
+#         # http://www.crummy.com/software/BeautifulSoup/bs4/doc/#next-sibling-and-previous-sibling
+#         anchor = anchor.next_sibling.next_sibling
+#         address = anchor.span.next.string.strip()
+
+#         anchor = anchor.next_sibling.next_sibling
+#         phone = anchor.span.next.string.strip()
+
+#         anchor = description.find('div', class_='block')
+#         open_time = anchor.find('ul', class_='bullet').li.string
+
+#         # Skip phần comment (<!-- /.block -->)
+#         anchor = anchor.next_sibling.next_sibling.next_sibling
+#         price_range = anchor.find('strong').string
+
+#         return Diner(name, address, phone,
+#                      category, open_time, price_range)
+
+#     def crawl(self):
+#         data = {
+#             'ajax': 1,
+#             'offset': 0,
+#             'areaid': 3,
+#             'areaseo': 'tp-ho-chi-minh'
+#         }
+
+#         offset = 0
+
+#         while True:
+#             response = self.session.post(
+#                 'http://diadiemanuong.com/location/ajaxLoadMore/', data=data)
+
+#             if response.status_code != 200:
+#                 return
+
+#             for diner in self.extract_page_data(response):
+#                 yield diner
+
+#             offset += 10
+#             data['offset'] = offset
