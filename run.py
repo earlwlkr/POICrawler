6eb7ccf8ad692d5cd8e30ee95503212d190f6be5
earlwlkr <mike358015@gmail.com>
2015-05-01 10:07:00 +0700

    Move run script to run.py

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
+from POICrawler.diners_crawler import FoodyVNCrawler
+
+def main():
+    LIMIT = 500
+
+    diners_crawler = FoodyVNCrawler()
+    diners_crawler.crawl()
+    out = open('example_output.txt', 'w', encoding='utf-8')
+    count = 0
+    for diner in diners_crawler.crawl():
+        out.write(diner.get_name() + '\n' +
+                  diner.get_address() + '\n' +
+                  diner.get_phone() + '\n' +
+                  diner.get_category() + '\n' +
+                  diner.get_open_time() + '\n' +
+                  diner.get_price_range() + '\n\n')
+        count += 1
+        print('{} diners collected.'.format(count))
+        if count >= LIMIT:
+            break
+    out.close()
+
+
 if __name__ == '__main__':
-    import POICrawler
-    POICrawler.main()
+    main()
