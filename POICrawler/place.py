5e362d415b7618b72479f45ed94e516f1b4ba398
earlwlkr <mike358015@gmail.com>
2015-05-01 11:12:51 +0700

    Add get category string

--- POICrawler/place.py
+++ POICrawler/place.py
@@ -1,6 +1,6 @@
 class Place(object):
 
-    def __init__(self, name='', address='', phone='', category=''):
+    def __init__(self, name='', address='', phone='', category=None):
         self.name = name
         self.address = address
         self.phone = phone
@@ -17,3 +17,12 @@ class Place(object):
 
     def get_category(self):
         return self.category
+
+    def get_category_string(self):
+        if self.category == 3:
+            return 'Nhà hàng'
+        if self.category == 4:
+            return 'Cà phê/Kem'
+        if self.category == 5:
+            return 'Quán ăn'
+        return 'Undefined ' + str(self.category)
