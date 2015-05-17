a1350d3d573c3f1dd1c6d34bac527b1572499978
earlwlkr <mike358015@gmail.com>
2015-05-17 12:48:55 +0700

    Add extract_options.py

---
+++ extract_options.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+
+def main():
+    client = MongoClient()
+    db = client.cityhotspots
+    db.drop_collection('dineroptions')
+    diners_collection = db.diners
+
+    doc = {}
+    diner_options_collection = db.dineroptions
+    doc['categories'] = diners_collection.distinct('category')
+    doc['cuisines'] = diners_collection.distinct('cuisine')
+    doc['districts'] = diners_collection.distinct('district')
+    diner_options_collection.insert(doc)
+
+
+if __name__ == '__main__':
+    main()
