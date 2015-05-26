50e956f85d2e4bd70ccafb15dcdbaeb518f77b5a
earlwlkr <mike358015@gmail.com>
2015-05-26 20:30:07 +0700

    Update run.py
    Always save to database, each crawl operation called by respective method

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from POICrawler.diners_crawler import FoodyVNCrawler
+from POICrawler.cinemas_crawler import CinemaCrawler
 from POICrawler.diner import Diner
 
 
@@ -8,6 +9,7 @@ def encode_diner(diner):
     return {
         "_type": "diner",
         "foody_id": diner.get_foody_id(),
+        "rating": diner.get_foody_rating(),
         "name": diner.get_name(),
         "address": {
             "street_address": address.get_street_address(),
@@ -25,43 +27,64 @@ def encode_diner(diner):
     }
 
 
-def main():
-    LIMIT = 28000
-    SAVE_TO_DATABASE = True
+def encode_cinema(cinema):
+    address = cinema.get_address()
+    return {
+        "_type": "cinema",
+        "foody_id": cinema.get_foody_id(),
+        "rating": cinema.get_foody_rating(),
+        "name": cinema.get_name(),
+        "address": {
+            "street_address": address.get_street_address(),
+            "district": address.get_district(),
+            "city": address.get_city(),
+            "country": address.get_country()
+        },
+        "category": cinema.get_category(),
+        "phone": cinema.get_phone(),
+        "trademark": cinema.get_trademark()
+    }
 
-    if SAVE_TO_DATABASE:
-        client = MongoClient()
-        db = client.cityhotspots
-        db.drop_collection('diners')
-        diners_collection = db.diners
+
+def main():
+    DINERS_LIMIT = 28000
+    crawl_diners(DINERS_LIMIT)
+    crawl_cinemas
+
+
+def crawl_diners(limit):
+    client = MongoClient()
+    db = client.cityhotspots
+    db.drop_collection('diners')
+    diners_collection = db.diners
 
     diners_crawler = FoodyVNCrawler()
-    out = open('example_output.txt', 'w', encoding='utf-8')
     count = 0
 
     for diner in diners_crawler.crawl():
         count += 1
-        out.write('STT: {}'.format(count) + '\n' +
-                  'Foody ID: ' + str(diner.get_foody_id()) + '\n' +
-                  'Tên: ' + diner.get_name() + '\n' +
-                  'Địa chỉ: ' + str(diner.get_address()) + '\n' +
-                  'SĐT: ' + diner.get_phone() + '\n' +
-                  'Danh mục: ' + diner.get_category() + '\n' +
-                  'Nền ẩm thực: ' + diner.get_cuisine() + '\n' +
-                  'Giờ mở cửa: ' + diner.get_open_time_string() + ' - ' +
-                  diner.get_close_time_string() + '\n' +
-                  'Khoảng giá: ' + str(diner.get_price_min()) + ' - ' +
-                  str(diner.get_price_max()) + '\n\n')
 
-        if SAVE_TO_DATABASE:
-            obj = encode_diner(diner)
-            diners_collection.replace_one({'foody_id': diner.get_foody_id()}, obj, upsert=True)
-            #diners_collection.insert(encode_diner(diner))
+        obj = encode_diner(diner)
+        diners_collection.replace_one(
+            {'foody_id': diner.get_foody_id()}, obj, upsert=True)
 
         print('{} diners collected.'.format(count))
-        if count >= LIMIT:
+        if count >= limit:
             break
-    out.close()
+
+
+def crawl_cinemas():
+    client = MongoClient()
+    db = client.cityhotspots
+    db.drop_collection('cinemas')
+    cinemas_collection = db.cinemas
+
+    cinemas_crawler = CinemaCrawler()
+    for cinema in cinemas_crawler.crawl():
+        obj = encode_cinema(cinema)
+        cinemas_collection.replace_one(
+            {'foody_id': cinema.get_foody_id()}, obj, upsert=True)
+
 
 if __name__ == '__main__':
     main()
